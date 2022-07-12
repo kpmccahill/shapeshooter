b8037d6f485fc1029f3d98b2ce4b99ce5cde6e4f
Kyle McCahill <kylemccahill@gmail.com>
2022-07-11 20:40:15 -0500

    test

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -1,14 +1,10 @@
+# The Player Character
 extends Sprite
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
