cdb49d78f5e2d8adc60c93e4b7c6cf375a0b3db8
Kyle McCahill <kylemccahill@gmail.com>
2022-07-12 12:28:39 -0500

    test

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -1,12 +1,36 @@
 # The Player Character
 extends Sprite
 
+export var speed = 200
+var win_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+		win_size = get_viewport_rect().size # Get a Vec2 containing the widow dimensions
+		position = win_size / 2 # Position the player at the center of the screen
+
+
+## Input handling
+#func _input(event):
+#		if event.is_action_pressed("move_right"):
+#			position.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+        position += velocity * delta
+        position.y = clamp(position.x, 0, win_size.x)
+        position.x = clamp(position.x, 0, win_size.x)
